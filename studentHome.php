b20943815a4574bf3de8a568f8cfc8b39fef2e36
Oliver Dorrian <dorrianolfc@gmail.com>
2022-05-19 10:21:36 +0100

    Fixed side scroll of Quizz#

--- studentHome.php
+++ studentHome.php
@@ -159,8 +159,7 @@
 
         <div id="calendarBig">
             <div id="callenderContainer">
-                <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=Europe%2FLondon&mode=WEEK&src=aGo1Y25yczZoN2w4Y3VuZHQ2dDF1dWR2bjBAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23F4511E" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>
-            </div>
+            <iframe src="https://calendar.google.com/calendar/embed?height=400&wkst=2&bgcolor=%23616161&ctz=Europe%2FLondon&showTz=0&showCalendars=0&showTabs=1&showPrint=0&showDate=0&showNav=0&showTitle=0&src=aGo1Y25yczZoN2w4Y3VuZHQ2dDF1dWR2bjBAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23F4511E" style="border-width:0" width="400" height="400" frameborder="0" scrolling="no"></iframe>            </div>
         </div>
 
         <div id="gradesBig">
